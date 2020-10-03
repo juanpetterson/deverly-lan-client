b6721b608061cb826cd253c51acc163324b6dc44
juanpetterson <juan.petterson@gmail.com>
2020-10-03 19:29:55 -0300

    add route for media streaming
    closes #7

--- server/src/index.js
+++ server/src/index.js
@@ -1,50 +1,52 @@
-const express = require('express');
-const socket = require('socket.io');
+const express = require("express");
+const socket = require("socket.io");
+const videoStreamRoutes = require("./routes/videoStreamRoutes/video");
 
 // App setup
 const PORT = 5000;
 const app = express();
-const server = app.listen(PORT, function () {
+
+// Routes
+app.use("/api", videoStreamRoutes);
+
+const server = app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
   console.log(`http://localhost:${PORT}`);
 });
-
-// Static files
-app.use(express.static('public'));
 
 // Socket setup
 const io = socket(server);
 
 const connectedUsers = new Map();
 
-io.on('connection', function (socket) {
+io.on("connection", (socket) => {
   const { user } = socket.handshake.query;
 
   connectedUsers.set(socket.handshake.query.user, socket);
   console.log(`New connection: ${user}`);
   console.log(socket.handshake.query.user);
 
-  socket.on('controller-play-pause', data => {
+  socket.on("controller-play-pause", (data) => {
     console.log(data);
 
-    io.emit('controller-play-pause', 'from server');
+    io.emit("controller-play-pause", "from server");
   });
 
-  socket.on('controller-stop', data => {
+  socket.on("controller-stop", (data) => {
     console.log(data);
 
-    io.emit('controller-stop', 'from server');
+    io.emit("controller-stop", "from server");
   });
 
-  socket.on('controller-back', data => {
+  socket.on("controller-back", (data) => {
     console.log(data);
 
-    io.emit('controller-back', 'from server');
+    io.emit("controller-back", "from server");
   });
 
-  socket.on('controller-forward', data => {
+  socket.on("controller-forward", (data) => {
     console.log(data);
 
-    io.emit('controller-forward', 'from server');
+    io.emit("controller-forward", "from server");
   });
 });
