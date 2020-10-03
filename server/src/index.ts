e4559add7981e1ab9e141d9704da884cddb783e3
juanpetterson <juan.petterson@gmail.com>
2020-10-03 20:12:52 -0300

    configure typescript and eslint on server
    closes #10

---
+++ server/src/index.ts
@@ -0,0 +1,53 @@
+import express from 'express';
+import socketio from 'socket.io';
+
+import videoStreamRoutes from './routes/videoStreamRoutes/video';
+
+// App setup
+const PORT = 5000;
+const app = express();
+
+// Routes
+app.use('/api', videoStreamRoutes);
+
+const server = app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+  console.log(`http://localhost:${PORT}`);
+});
+
+// Socket setup
+const io = socketio(server);
+
+const connectedUsers = new Map();
+
+io.on('connection', (socket) => {
+  const { user } = socket.handshake.query;
+
+  connectedUsers.set(socket.handshake.query.user, socket);
+  console.log(`New connection: ${user}`);
+  console.log(socket.handshake.query.user);
+
+  socket.on('controller-play-pause', (data) => {
+    console.log(data);
+
+    io.emit('controller-play-pause', 'from server');
+  });
+
+  socket.on('controller-stop', (data) => {
+    console.log(data);
+
+    io.emit('controller-stop', 'from server');
+  });
+
+  socket.on('controller-back', (data) => {
+    console.log(data);
+
+    io.emit('controller-back', 'from server');
+  });
+
+  socket.on('controller-forward', (data) => {
+    console.log(data);
+
+    io.emit('controller-forward', 'from server');
+  });
+});
