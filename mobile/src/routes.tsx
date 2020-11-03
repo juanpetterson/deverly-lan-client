d1ef483f20ecbc3774d60b4dfe25650087f38434
Juan Petterson Heberle <juan.petterson@gmail.com>
2020-11-02 21:52:34 -0300

    add bottom tab navigator

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -1,18 +1,48 @@
 import * as React from 'react';
+import { Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import PlayerPage from './pages/PlayerPage';
-import Gallery from './pages/Gallery';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FeatherIcons from 'react-native-vector-icons/Feather';
 
-const Stack = createStackNavigator();
+import PlayerPage from './pages/PlayerPage';
+
+const Tab = createMaterialBottomTabNavigator();
+
+const EmptyComponent = () => {
+  return <Text>EmptyComponent</Text>;
+};
 
 function Routes(): JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={PlayerPage} />
-        <Stack.Screen name="Gallery" component={Gallery} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        initialRouteName="Player"
+        activeColor="#f0edf6"
+        inactiveColor="#aaaaaa"
+        barStyle={{ backgroundColor: '#282828' }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={PlayerPage}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={EmptyComponent}
+          options={{
+            tabBarLabel: 'Search',
+            tabBarIcon: ({ color }) => (
+              <FeatherIcons name="search" color={color} size={26} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
