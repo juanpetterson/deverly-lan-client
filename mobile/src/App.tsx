6731f29ece02db1d3558b7a9434b90208fd7e53e
Juan Petterson Heberle <juan.petterson@gmail.com>
2020-11-06 23:02:18 -0300

    configure fonts and status bar color

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,6 +1,31 @@
+/* eslint-disable camelcase */
 import React from 'react';
+import { AppLoading } from 'expo';
+import { StatusBar } from 'expo-status-bar';
+
+import {
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
 import Routes from './routes';
 
 export default function App(): JSX.Element {
-  return <Routes />;
+  const [fontsLoaded] = useFonts({
+    roboto_400: Roboto_400Regular,
+    roboto_500: Roboto_500Medium,
+    roboto_700: Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <>
+      <Routes />
+      <StatusBar style="light" />
+    </>
+  );
 }
