f2dc6161415056b328833e468a1a8d440c114f9f
Juan Petterson Heberle <juan.petterson@gmail.com>
2020-11-07 21:34:32 -0300

    remove unnecessary lines

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-// import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
@@ -11,7 +10,6 @@ import Gallery from './pages/Gallery';
 import colors from './styles/colors';
 
 const { Navigator, Screen } = createBottomTabNavigator();
-// const { Navigator, Screen } = createMaterialBottomTabNavigator();
 
 function Routes(): JSX.Element {
   return (
