4c6ff6520853b127a0c06a220c0a34dfffa297b9
Juan Petterson Heberle <juan.petterson@gmail.com>
2020-11-18 22:35:55 -0300

    remove socket-io usage

--- mobile/src/pages/PlayerPage/index.tsx
+++ mobile/src/pages/PlayerPage/index.tsx
@@ -1,10 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { View } from 'react-native';
 import { Video } from 'expo-av';
 
-import socket from '../../services/websockets';
 import Player from '../../components/Player';
-import { VideoStatus } from '../../components/Player/types.interface';
 
 interface IMedia {
   path: string;
@@ -16,37 +14,6 @@ const PlayerPage: React.FC = () => {
   const [currentMedia, setCurrentMedia] = useState('suits');
 
   const playerRef = useRef<Video>(null);
-  useEffect(() => {
-    socket.on('controller-play-pause', async () => {
-      const status = (await playerRef.current?.getStatusAsync()) as VideoStatus;
-
-      if (status.isPlaying) {
-        await playerRef.current?.pauseAsync();
-      } else {
-        await playerRef.current?.playAsync();
-      }
-    });
-
-    socket.on('controller-stop', async () => {
-      await playerRef.current?.stopAsync();
-    });
-
-    socket.on('controller-back', async () => {
-      const status = (await playerRef.current?.getStatusAsync()) as VideoStatus;
-
-      await playerRef.current?.setPositionAsync(status.positionMillis - 10000);
-    });
-
-    socket.on('controller-forward', async () => {
-      const status = (await playerRef.current?.getStatusAsync()) as VideoStatus;
-
-      await playerRef.current?.setPositionAsync(status.positionMillis + 10000);
-    });
-
-    socket.on('controller-media-change', async (data: IMedia) => {
-      setCurrentMedia(data.path);
-    });
-  }, []);
 
   return (
     <View style={{ flex: 1 }}>
--- mobile/src/services/websockets.ts
+++
@@ -1,5 +0,0 @@
-import io from 'socket.io-client';
-
-export default io(`http://10.0.3.2:5000`, {
-  query: { user: 'logged user' },
-});
